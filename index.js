89d5211fd980b5df673508f2824b6ce12c8dadf8
Chiaen Fu <peterfuwork@gmail.com>
2018-12-06 16:29:05 -0700

    added more routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
+var randomString = require('random-string');
 const PORT = process.env.PORT || 4000;
 const queries = require('./queries');
 
@@ -26,7 +27,38 @@ app.get('/movies', (req, res) => {
 app.get('/movies/m_id/:id', (req, res) => {
     queries.getById(req.params.id).then(movie => {
         res.send({ movie });
+    });
+})
+
+app.post('/moviePOST', (req, res, next) => {
+    console.log('req.body', req.body)
+    const body = req.body;
+    const randomStr = randomString({
+        length: 10,
+        numeric: true,
+        letters: true,
+        special: false
+    });
+    const newObj = {
+        m_id: randomStr,
+        name: body.name,
+        desc: body.desc,
+        type: body.type,
+        image: body.image,
+        director: body.director,
+        year: Number(body.year),
+        stars: body.stars,
+        length_min: body.length,
+        MPAA: body.MPAA
+    };
+    queries.createMovie(newObj).then(Movie => res.send(Movie))
+})
+
+app.delete('/movieDELETE/m_id/:id', (req, res, next) => {
+    queries.deleteMovie(req.params.id).then(Movie => {
+        res.send(Movie)
     })
+    .then(res.sendStatus(204))
 })
 
 
--- queries.js
+++ queries.js
@@ -7,12 +7,12 @@ module.exports = {
     getById(id){
         return db.select().from('movies').where('m_id', id);
     },
-    // createStudent(newStudent){
-    //     return db('students').insert(newStudent).returning('*');
-    // },
-    // deleteStudent(id){
-    //     return db.delete().from('students').where('id', id).returning('*');
-    // },
+    createMovie(newMovie){
+        return db('movies').insert(newMovie).returning('*');
+    },
+    deleteMovie(id){
+        return db.delete().from('movies').where('m_id', id).returning('*');
+    }
     // updateStudent(id, updateStudentInfo) {
     //     return db.update(updateStudentInfo).from('students').where('id', id).returning('*');
     // }
