b156a4728fb7f860d92975d3ef21de821707cf41
Chiaen Fu <peterfuwork@gmail.com>
2018-12-15 21:52:26 -0700

    updated post delete put route

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const randomString = require('random-string');
 const PORT = process.env.PORT || 4000;
 const queries = require('./queries');
 
@@ -30,15 +29,9 @@ app.get('/movies/m_id/:id', (req, res) => {
     });
 })
 
-app.post('/moviePOST', (req, res, next) => {
+app.post('/movies/moviePOST', (req, res, next) => {
     console.log('req.body', req.body)
     const body = req.body;
-    // const randomStr = randomString({
-    //     length: 10,
-    //     numeric: true,
-    //     letters: true,
-    //     special: false
-    // });
     const newObj = {
         name: body.name,
         desc: body.desc,
@@ -46,21 +39,26 @@ app.post('/moviePOST', (req, res, next) => {
         image: body.image,
         director: body.director,
         year: Number(body.year),
-        stars: body.stars,
+        stars: Number(body.stars),
         length_min: body.length,
         MPAA: body.MPAA
     };
+    console.log(newObj);
     queries.createMovie(newObj)
-    .then(Movie => res.send(Movie))
+    .then(movie => {
+        res.send({status: 200, message: 'success!', movie});
+    });
+    
 })
 
-app.put('/moviePUT', (req, res, next) => {
+app.put('/movies/moviePUT', (req, res, next) => {
+    console.log(req.body)
     const body = req.body;
     queries.updateMovie(body.m_id, body)
     .then(Movie => res.send(Movie))
 });
 
-app.delete('/movieDELETE', (req, res, next) => {
+app.delete('/movies/movieDELETE', (req, res, next) => {
     queries.deleteMovie(req.body.m_id)
     .then(Movie => {
         res.status(200).send(Movie);
@@ -68,11 +66,28 @@ app.delete('/movieDELETE', (req, res, next) => {
     
 })
 
-app.get('/movies/test/', (req, res, next) => {
-    console.log(req.query);
-    const newObj = { type: req.query.type, stars: Number(req.query.stars)};
-    res.send(newObj);
-});
+app.get(`/movies/find/`, (req, res, next) => {
+    const { per, page } = req.query;
+    queries.getAllMovies()
+    .then(movies => {
+        const indexOfLastMovie = page * per;
+        const indexOfFirstMovie = indexOfLastMovie - per;
+        const currentMovie = movies.slice(indexOfFirstMovie, indexOfLastMovie);
 
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(movies.length / per); i++) {
+            pageNumbers.push(i);
+        }
+        const total_pages = pageNumbers[pageNumbers.length - 1];
+        // console.log('total_pages',total_pages)
+        const newObj = {
+            per: Number(per),
+            page: Number(page),
+            total_pages,
+            currentMovie
+        }
+        res.send(newObj);
+    });
+});
 
 app.listen(PORT);
